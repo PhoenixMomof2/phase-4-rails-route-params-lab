37fbc6a505f9f6a0a953e0f681579e8a3f6289f3
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-01-24 10:50:40 -0500

    commit tree 84b827ca363a2a14c68eeed7045d6ce6aba64e34

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  # def index
+  #   student = Student.where(first_name: params[:first_name] || last_name: params[:last_name])
+  #   render json: student
+  # end
+
 end
